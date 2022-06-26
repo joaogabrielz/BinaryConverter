6673b37a2e0784960307d4a0f32d60bc24cb0455
@joaogabrielz <89491372+joaogabrielz@users.noreply.github.com>
2022-06-26 10:19:34 -0400

    Delete BinaryConverter directory

--- BinaryConverter/js/recursion.js
+++
@@ -1,9 +0,0 @@
-function convertToBinary (dec) {
-    if (dec > 0) {
-        return convertToBinary( parseInt(dec / 2) ) + (dec % 2)
-    }
-    if(dec == 0) return 0;
-    
-    return '';
-}
-console.log(convertToBinary(0));
